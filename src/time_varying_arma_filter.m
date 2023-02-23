bf92fd77d22b801bf316e77581ea73b655906fc7
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-02-23 04:17:54 +0300

    feat(tv-arma): convert to parallel implementation
    Convert to personal time-varying ARMA filter implementation base to
    parallel ARMA filter implementation.
    
    Refs: #3

--- src/time_varying_arma_filter.m
+++ src/time_varying_arma_filter.m
@@ -1,14 +1,10 @@
-function Y = time_varying_arma_filter(M, b, a, X, T, tol)
-%FILTER_ARMA Simulates a direct ARMA graph filter under a static signal and graph. 
-% 
+function [Z, psi, phi] = agsp_filter_ARMA_parallel(M, b, a, X, T, verbose)
 % INPUT ARGUMENTS
 %   M:   This is the shifted Laplacian matrix M = 0.5*norm(L)I - L,
 %        where L is the combinatorial/normalized Laplacian. 
 %        (Shifting the Laplacian helps the stability.) 
 %        
-%   X:   The time-varying graph signal to be filtered.
-% 
-%   b, a: are the coefficient of the numerator and denominator polynomials 
+%   b,a: are the coefficient of the numerator and denominator polynomials 
 %        of the filter's spectral response:
 %        $$r(mu) = \frac{ sum_{j=0}^{K-1} b(j+1) mu^{j} }{ sum_{j=0}^{K}
 %        a(j+1) mu^{j} }$$.
@@ -16,58 +12,84 @@ function Y = time_varying_arma_filter(M, b, a, X, T, tol)
 %        since M = 0.5*norm(L)I - L, the polynomials should also
 %        be expressed in terms of the eigenvalues mu of M.
 % 
+%  X:   The JTV signal to be filtered.
+% 
 %  T:    How many iterations should the ARMA recursions be run for?
 % 
 %  verbose: Set to 1 to see more information, 0 otherwize
 % 
 % OUTPUT ARGUMENTS
 % 
-%  Y:    The filtered signal
+%  Z:    The filtered signal
+% 
+%  phi,psi: The coefficients of the parallel ARMA recursions.
 % 
 % Tuna Alikaşifoğlu
 % February 2023
 
-if ~exist('tol', 'var'), tol = 1e-4; end
-x = X(:, 1);
+if ~exist('verbose', 'var'), verbose = 0; end
 
-% normalize such that a(1) = 1
-a = a / a(1);
-b = b / a(1);
+n = size(M, 1);
 
-n = size(M,1); Ka = numel(a)-1; Kb = numel(b)-1;
+% first compute the phi/psi coefficients
+num = b; den = a;
 
-y = zeros(n,T);
+% partial fraction expansion
+[residues,poles,addTerm] = residue(wrev(num), wrev(den));
+
+if isempty(addTerm), addTerm = 0; end
+if addTerm ~= 0, disp('warning: addTerm non zero'); addTerm = sum(addTerm); end
+
+
+% recursion coefficients (y_{t+1} = psi M y_{t} + phi x)
+K = numel(poles);
+psi = zeros(K,1); phi = zeros(K,1);
+for j = 1:K,
+    psi(j) = 1/poles(j);
+    phi(j) = -residues(j)/poles(j);
+end
+
+% test for stability
+if verbose,
+    poles = 1./psi;
+    if min(abs(poles)) > norm(full(M)),
+        disp('filter is stable');
+    else
+        disp('warning: filter is unstable. Unexpected behavior expected!');
+    end
+end
+
+x = X(:, 1); % for test output added for jtv
+
+% parallel ARMA recursion
+if size(x,2) == 1, x = repmat(x,1,T); end
+y = zeros(n, K, T+1); y(:,:,1) = repmat(x(:,1), 1, K);
+z = zeros(n,T+1); z(:,1) = x(:,1);
 for t = 1:T,
-    
-    y(:,t) = 0;
-            
-    % AR terms
-    for k = 1:Ka
-        if t - 1 > 0,
-            if k == 1, 
-                z = y(:,t-1); 
-            end
-            z = M * z;
-            y(:,t) = y(:,t) - a(k+1)*z;
-        end
+    for j = 1:K,
+        y(:,j,t+1) = psi(j) * M * y(:,j,t) + phi(j) * x(:,t);
     end
-
-    % MA terms
-    z = x;
-    for k = 0:Kb
-         y(:,t) = y(:,t) + b(k+1)*z;
-         z = M*z;
-    end
-    
-    if t > 1 && norm(y(:,t) - y(:,t-1))/norm(y(:,t-1)) < tol, break; end
-end
-y = y(:,1:t);
-
-Y = zeros(size(X));
-for i = 1:size(X,2)
-    Y(:,i) = y(:,end);
+    z(:,t+1) = sum(y(:,:,t+1), 2) + addTerm*x(:,t);
 end
 
+Z = repmat(z(:,end),1, size(X,2)); % for test output added for jtv
+
+% plot
+if verbose, 
+    response = @(mu) polyval(wrev(b), mu) ./ polyval(wrev(a), mu);
+    [U, mu] = eig(M, 'vector'); [mu, idx] = sort(mu); U = U(:,idx);
+    
+    x_GFT = U' * x(:,1);
+    z_GFT = U' * z(:,end);
+    
+    figure; set(gcf, 'Position', [680 558 760 350]);
+    subplot(1,2,1); hold on;
+    plot(z'); xlabel('iteration'); ylabel('filter output');
+    subplot(1,2,2); hold on;
+    plot(mu, response(mu), 'k-', 'DisplayName', 'r(mu)');
+    plot(mu, abs(z_GFT./x_GFT), 'rx', 'DisplayName', 'abs(z_GFT./x_GFT)')
+    xlabel('mu'); ylabel('filter response');
+end
 end
 
 
