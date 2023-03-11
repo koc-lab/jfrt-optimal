9d508f82b34d9a0cc3d79d66799398bfebb93046
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-03-11 13:24:13 +0300

    feat: convert `chirp-arma` to result form
    With this conversion the ARMA results for the synthetic chirp denosing
    task has been obtained. The `chirp_arma.m` script is also converted to a
    form that generates the results that will be directly provided in the
    manuscript.
    
    Refs: #8
    Fixes: #9

--- denoising-task/chirp_arma.m
+++ denoising-task/chirp_arma.m
@@ -38,55 +38,49 @@ else
         [h, w] = freqz(b, a);
         hn = h / max(abs(h));
         [b, a] = invfreqz(hn, w, length(b), length(a));
+        % [b, a] = invfreqz(hn, w, length(b), length(a), [], 100); % stable version
     end
 end
 
 %% Noise Parameters
-mu    = 0;
-sigma = 0.1;
+sigmas = [0.1, 0.15, 0.2];
 
 %% Joint Time-Vertex Signal Generation
 fs   = 1000;
 fmax = 400;
 fc   = 450;
+delay_multipliers = [20, 25, 30, 35, 40, 45, 50];
 
 Ts = 1 / fs;
 t = 0:Ts:100 * Ts - Ts;
 T = length(t);
 
-% for delay_multiplier = [20, 25, 30, 35, 40, 45, 50]
-for delay_multiplier = [20]
-    delay_coeff = Ts * delay_multiplier;
-    delays = vecnorm(G.coords, 2, 2);
-    delays = delay_coeff * delays / max(delays);
+for delay_multiplier = delay_multipliers
+    for sigma = sigmas
+        delay_coeff = Ts * delay_multiplier;
+        delays = vecnorm(G.coords, 2, 2);
+        delays = delay_coeff * delays / max(delays);
 
-    X = zeros(N, T);
-    for i = 1:N
-        t_prime = t + delays(i);
-        X(i, :) = chirp(t_prime, 0, 1, fmax);
+        X = zeros(N, T);
+        for i = 1:N
+            t_prime = t + delays(i);
+            X(i, :) = chirp(t_prime, 0, 1, fmax);
+        end
+
+        % Add Noise
+        rng("default");
+        noise = sigma * randn(size(X));
+        X_noisy = X + noise;
+
+        % Experiment
+        fprintf("sigma = %.2f, delay = %d\n", sigma, delay_multiplier);
+        noise_error = 100 * norm(X - X_noisy, "fro") / norm(X, "fro");
+        fprintf("\tNoise error: %g%%\n", noise_error);
+
+        Y = time_varying_arma_filter(M, b, a, X_noisy);
+        Y = real(Y);
+        filter_err = norm(X - Y, 'fro') / norm(X, 'fro');
+        fprintf("\tEstimation Error: %g%%\n", filter_err * 100);
     end
-
-    % Add Noise
-    rng("default");
-    noise = mu + sigma * randn(size(X));
-    X_noisy = X + noise;
-
-    % Experiment
-    noise_error = 100 * norm(X - X_noisy, "fro") / norm(X, "fro");
-    fprintf("Noise error: %g%%\n", noise_error);
-
-    Y = time_varying_arma_filter(M, b, a, X_noisy);
-    Y = real(Y);
-    filter_err = norm(X - Y, 'fro') / norm(X, 'fro');
-    fprintf("Estimation Error: %g%%\n", filter_err * 100);
-
-    index = 50;
-    figure;
-    title(sprintf("Time-Series Signal at Node %d", index));
-    plot(X(index, :), 'LineWidth', 2);
-    hold on;
-    plot(X_noisy(index, :), 'LineWidth', 2);
-    plot(Y(index, :), 'LineWidth', 2);
-    legend("Original", "Noisy", "Filtered");
 end
 
