448ce11720db93acdd61a918f1c38652a0bf07ea
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-03-15 18:43:42 +0300

    feat: update real-world `main` driver
    Add utilization of sea temperature data and a sample filtering.

--- real-world/main.m
+++ real-world/main.m
@@ -1,70 +1,60 @@
 %% Clear
 clc, clear, close all;
 
-%% Load GSP Toolbox
-GSP_TOOLBOX_PATH = "/path/to/gsp/toolbox";
-addpath(GSP_TOOLBOX_PATH);
-gsp_start;
+%% Load data
+[G, X] = init_knn("sea-surface-temperature.mat", 10, 6);
+T = 120;
+X = X(:, 1:T);
+X = X / max(X(:));
 
-%% Parameters
+% Add Noise
 rng("default");
-
-gft_methods = ["adj", "lap"];
-alphas = linspace(0.9, 1.1, 99);
-betas = linspace(0.0, 3.0, 199);
-
-if ~ismember(1.0, alphas)
-    alphas = [alphas, 1.0];
-end
-
-if ~ismember(1.0, betas)
-    betas = [betas, 1.0];
-end
-
-%% Generation of Graph
-N = 64;
-graph_lpf_count = 1;
-no_glpf_error = 3.42404;
-% no_glpf_error = -1;
-
-% G = gsp_sensor(N);
-G = gsp_david_sensor_network(N);
-figure;
-gsp_plot_graph(G);
-
-%% Joint Time-Vertex Signal Generation
-fs = 1000;
-Ts = 1 / fs;
-t = 0:Ts:100 * Ts - Ts;
-T = length(t);
-
-fmin = 20;
-fmax = 20;
-fc = fmax * 1.5;
-f_values = round(linspace(fmin, fmax, N));
-
-X = zeros(N, T);
-for i = 1:length(f_values)
-    X(i, :) = cos(2 * pi * f_values(i) * t);
-end
-plot_jtv_signal_time(X, t, "JTV Signal", "x", 1);
-
-%% Add Noise
 mu    = 0;
 sigma = 0.1;
+noise = mu + sigma * randn(size(X));
+X_noisy = X + noise;
 
-if true
-    noise = mu + sigma * randn(size(X));
-else
-    noise = mu + sigma * cos(2 * pi * 3 * fmax * t);
+time_count = 60;
+time_filter = ones(size(X, 2), 1);
+time_filter(end-time_count+1:end) = 0;
+Ht = diag(time_filter);
+
+graph_count = 20;
+graph_filter = ones(size(X, 1), 1);
+graph_filter(end-graph_count+1:end) = 0;
+Hg = diag(graph_filter);
+
+for method = ["adj"]
+    for alpha = [1.0]
+        for beta = [1.0]
+            [gft_mat, ~] = gft_matrix(full(G.W), method);
+            jfrt_pair = get_jfrt_pair(gft_mat, T, alpha, beta);
+
+            X_transform = jfrt_pair("GFRT") * X_noisy * jfrt_pair("FRT_T");
+            X_transform_filtered = Hg * X_transform * Ht;
+            X_filtered = real(jfrt_pair("IGFRT") * X_transform_filtered * jfrt_pair("IFRT_T"));
+        end
+    end
 end
 
-X_noisy = X + noise;
-plot_jtv_signal_time(X_noisy, t, "Noisy JTV Signal", "x^{(noisy)}", 1);
+%% SNR
+noise_snr    = 10 * log10(norm(X, "fro")^2 / norm(X - X_noisy, "fro")^2);
+filtered_snr = 10 * log10(norm(X, "fro")^2 / norm(X - X_filtered, "fro")^2);
 
-%% Experiment
-noise_error = 100 * norm(X - X_noisy, "fro") / norm(X, "fro");
-fprintf("Noise error: %g\n", noise_error);
-fprintf("Error with no GLPF: %g\n", no_glpf_error);
-experiment(G, t, X, X_noisy, gft_methods, alphas, betas, graph_lpf_count, fs, fc);
+fprintf("SNR before filtering: %.2f dB\n", noise_snr);
+fprintf("SNR after  filtering: %.2f dB\n", filtered_snr);
+
+%% Plot
+figure;
+plot(X(1, :), "LineWidth", 2);
+hold on;
+plot(X_noisy(1, :), "LineWidth", 2);
+legend("Original", "Noisy");
+
+figure;
+plot(X(1, :), "LineWidth", 2);
+hold on;
+plot(X_filtered(1, :), "LineWidth", 2);
+legend("Original", "Filtered");
+
 
