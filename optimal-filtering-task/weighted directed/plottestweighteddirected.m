0353c6d15454bd015c14d82364df8853b00455ac
Tuna Alikaşifoğlu <tunakasif@gmail.com>
2023-03-29 13:49:07 +0300

    feat: import submitted optimal filter experiment (#17)
    Import the submitted version of the optimal filtering
    task prepared by Eray.
    
    Resolves: #16

---
+++ optimal-filtering-task/weighted directed/plottestweighteddirected.m
@@ -0,0 +1,24 @@
+ close all;
+ 
+
+%     name = ['data/rng5-k5-adj-eig-weighteddirected'];
+%     name = ['data/eig2ptsnewesttestwd5']; 
+    name = ['data/fixednode.mat'];
+    load( name);
+    figure()
+    MSE = real(MSE);
+    a = 0:0.1:2;
+    b = 0:0.1:1;
+    % MSE for directed graph with independent time
+    p = surf(a,b,MSE);
+    colormap('gray');
+    z = min(min(MSE));
+    [x,y] = find(MSE==z);
+    datatip(p,'DataIndex', find(MSE==z));
+    p.DataTipTemplate.DataTipRows(1) = ['Graph fraction: ',num2str(a(y))];
+    p.DataTipTemplate.DataTipRows(2) = ['Time fraction: ',num2str(b(x))];
+    p.DataTipTemplate.DataTipRows(3) = ['MSE: ', num2str(z)];
+    ylabel("Time fraction")
+    xlabel("Graph fraction")
+    zlabel("MSE")
+ saveas(gcf,'figs/fixednode', 'epsc')
